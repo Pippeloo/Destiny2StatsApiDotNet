824eb2403f3e979b79ed36670651b70b6734f7cf
Pippeloo <68497705+Pippeloo@users.noreply.github.com>
2022-12-08 11:01:02 +0100

    Saving first working version

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Destiny2StatsApiDotNet.Models;
+using Contentful.Core;
+
+namespace Destiny2StatsApiDotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IContentfulClient _client;
+
+        public HomeController(ILogger<HomeController> logger, IContentfulClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _client.GetEntries<Destiny2StatsTracker>();
+            return View(user);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Destiny2StatsTracker.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Destiny2StatsApiDotNet.Models
+{
+    public class Destiny2StatsTracker
+    {
+        public string Username { get; set; }
+
+        public bool boolean { get; set; }
+
+        public string Title { get; set; }
+    }
+}
